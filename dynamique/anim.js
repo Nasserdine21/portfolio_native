ff6b8d1d11073b6755a55d39244768186d7a8320
Nasserdine21 <nasserkrauser@gmail.com>
2021-10-05 17:10:22 +0100

    navbar_error

---
+++ dynamique/anim.js
@@ -0,0 +1,11 @@
+let menu = document.querySelector(".burger");
+let show = document.querySelector("#bg");
+
+
+menu.addEventListener('click', () => {
+
+    
+
+    menu.classList.add("menurotate");
+    show.classList.add("show");
+});
